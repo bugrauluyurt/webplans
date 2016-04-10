da562506cf41ff49b050a74f770141ae03595d27
bugrauluyurt <bugra.u@gmail.com>
2016-04-10 16:57:12 +0300

    Finished default admin user

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,5 +1,28 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  if (Meteor.users.find().count() < 1) {
+ 	var users = [
+ 		{
+ 			name: 'Superuser',
+ 			email: 'admin@example.com',
+ 			roles: ['admin']
+ 		}
+ 	];
+ 		
+ 	_.each(users, function(user){
+ 		var id;
+ 		id = Accounts.createUser({
+ 			email: user.email,
+ 			password: 'password',
+ 			profile: {
+ 				name: user.name
+ 			}
+ 		});
+
+ 		if (user.roles.length > 0) {
+ 			Roles.addUsersToRoles(id, user.roles);
+ 		}
+ 	});	
+  }
 });
